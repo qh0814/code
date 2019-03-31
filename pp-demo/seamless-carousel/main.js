6776469927fa9c688bbc05e8f76afd72d8de9c26
QiuHao <804271135@qq.com>
2019-03-31 15:05:15 +0800

    flex version

--- pp-demo/seamless-carousel/main.js
+++ pp-demo/seamless-carousel/main.js
@@ -4,32 +4,52 @@ $(document).ready(function(){
   var lastItem = $('.wraper a').last()
   $(lastItem).append(img)
   var size = $('.wraper img').length
-  console.log('共有' + size + '个图片')
+  //console.log('共有' + size + '个图片')
   $('li').first().addClass('active')
   //循环轮播事件
   var i = 4
   var ID = setInterval(() => {
     i++
-    // if(i === size){
-    // // $('.wraper ,.move').css({'transform':'translate(' + -i*500 + 'px)'})
-    // // $('.wraper').addClass('move')
-    // $('.wraper').css({'transform':'translate(' + size*0 + 'px)'})//首尾图片相同，视觉上不影响
-    // i = 1
-    // }
     move()
-  },3000)
+  },2000)
+// 悬停清除计时器，移除创建计时器
+$('.slider-list').hover(() => {
+  clearInterval(ID)
+},() =>{
+  ID = setInterval(() => {
+    i++
+    move()
+  },2000)
+})
 
+//用户手动选择
+$('.index li').hover( function() {
+  console.log(this)//如果用箭头函数this会指向document..
+  let currentIndex = $(this).index()
+  console.log(currentIndex)
+  $('.wraper').stop().animate({left:-currentIndex*300},500)
+  i = currentIndex
+  $(this).addClass('active').siblings().removeClass('active')
+})
+
+$('.leftButton').click(function(){
+  i--
+  move()
+})
+$('.rightButton').click(function(){
+  i++
+  move()
+})
   function move(){
-    console.log(i)
+    //console.log(i)
     if(i === size){
       $('.wraper').css({left:0})//首尾图片相同，视觉上不影响
       i = 1
-      console.log(size*0)
+      //console.log(size*0)
       //debugger
     }
     if(i === -1){
-      
-      $('.wraper').css({'transform':'translate(' + (size-1) *  (-300) + 'px)'})
+      $('.wraper').css({left:-(size-1)*300})
       i = size - 2//从第一个向左退到最后一个
     }
     
