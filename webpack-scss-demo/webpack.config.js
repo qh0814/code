7ad157371db9d5beec5594a3ccba8e568cbb60d0
QiuHao <804271135@qq.com>
2019-04-20 04:30:54 +0800

    webpack scss demo

---
+++ webpack-scss-demo/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  mode:'development',//开发环境
+  entry:'./src/index.js',//入口js
+  output:{
+    filename:'bundle.js',//打包后文件名
+    path:path.resolve(__dirname,'dist'),//打包后输出目录
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+      template:'./src/index.html',//模板html
+      filename:'index.html',//打包后文件名     
+    }),
+    new MiniCssExtractPlugin({
+      filename:'index.css',//生成的css，默认main.css
+    })
+  ],
+  module:{
+    rules:[
+      {
+        test:/\.scss$/,
+        use:[
+          MiniCssExtractPlugin.loader,
+          {loader:'css-loader'},
+          {loader:'sass-loader'}//loader从右向左执行
+        ]
+      }
+    ]
+  }
+
+}
